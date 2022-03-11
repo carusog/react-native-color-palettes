e042a667602c820fefd3e8e61b37f8bc1de3e555
Giuseppe Caruso <giuseppe.caruso@smartly.io>
2022-03-11 20:29:12 +0100

    add first layout exercise

--- App.js
+++ App.js
@@ -1,20 +1,70 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 
 export default function App() {
+  console.log('🪲', '🐞');
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.title}>
+          Here are some boxes of different colours
+        </Text>
+        <View style={[styles.cyan, styles.color]}>
+          <Text style={styles.colorText}>Cyan: #2aa198</Text>
+        </View>
+        <View style={[styles.blue, styles.color]}>
+          <Text style={styles.colorText}>Blue: #268bd2</Text>
+        </View>
+        <View style={[styles.magenta, styles.color]}>
+          <Text style={styles.colorText}>Magenta: #d33682</Text>
+        </View>
+        <View style={[styles.orange, styles.color]}>
+          <Text style={styles.colorText}>Orange: #cb4b16</Text>
+        </View>
+
+        <StatusBar style="auto" />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  title: {
+    fontWeight: '800',
+    marginVertical: 16,
+    alignSelf: 'flex-start',
+    fontSize: 16,
+  },
+  color: {
+    width: '100%',
+    marginBottom: 8,
+    padding: 10,
+    borderRadius: 4,
+  },
+  colorText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  cyan: {
+    backgroundColor: '#2aa198',
+  },
+  blue: {
+    backgroundColor: '#268bd2',
+  },
+  magenta: {
+    backgroundColor: '#d33682',
+  },
+  orange: {
+    backgroundColor: '#cb4b16',
+  },
+  safeArea: {
+    flexGrow: 1,
+  },
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
+    width: '100%',
+    flexGrow: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 24,
   },
 });
